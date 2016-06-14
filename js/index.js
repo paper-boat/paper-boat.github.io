0b709afbf0b83e8a011211502767ed31322ff984
paper-boat <879754072@qq.com>
2016-06-14 16:58:32 +0800

    sd

--- js/index.js
+++ js/index.js
@@ -68,6 +68,52 @@ $(function() {
       $('html,body').animate({scrollTop:$(this.hash).offset().top},1200);
     });
   });
+
   var chartcontent = $('.work-img');
   chartcontent.css('top','0');
+
+
+  var psaiXianshi = $('.hover-psai1');
+  psaiXianshi.on('click',function(){
+    $('.psai-img-fangda1').show();
+  })
+  $('.psai-close').on('click',function(){
+    $('.psai-img-fangda1').hide();
+  })
+  $('.psai-img-fangda1').on('click',function(){
+    $('.psai-img-fangda1').hide();
+  })
+
+  var psaiXianshi = $('.hover-psai2');
+  psaiXianshi.on('click',function(){
+    $('.psai-img-fangda2').show();
+  })
+  $('.psai-close').on('click',function(){
+    $('.psai-img-fangda2').hide();
+  })
+  $('.psai-img-fangda2').on('click',function(){
+    $('.psai-img-fangda2').hide();
+  })
+
+  var psaiXianshi = $('.hover-psai3');
+  psaiXianshi.on('click',function(){
+    $('.psai-img-fangda3').show();
+  })
+  $('.psai-close').on('click',function(){
+    $('.psai-img-fangda3').hide();
+  })
+  $('.psai-img-fangda3').on('click',function(){
+    $('.psai-img-fangda3').hide();
+  })
+
+  var psaiXianshi = $('.hover-psai4');
+  psaiXianshi.on('click',function(){
+    $('.psai-img-fangda4').show();
+  })
+  $('.psai-close').on('click',function(){
+    $('.psai-img-fangda4').hide();
+  })
+  $('.psai-img-fangda4').on('click',function(){
+    $('.psai-img-fangda4').hide();
+  })
 });
